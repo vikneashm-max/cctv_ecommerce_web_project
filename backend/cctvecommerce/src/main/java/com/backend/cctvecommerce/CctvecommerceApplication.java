510894d5ba61e45cbc1c28fd6911a34740d35fb3
vikneashm-max <vikneashm@gmail.com>
2026-05-23 19:32:02 +0530

    Structured like frontend and backend

---
+++ backend/cctvecommerce/src/main/java/com/backend/cctvecommerce/CctvecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.cctvecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctvecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CctvecommerceApplication.class, args);
+	}
+
+}
---
+++ backend/cctvecommerce/src/test/java/com/backend/cctvecommerce/CctvecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.cctvecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CctvecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
